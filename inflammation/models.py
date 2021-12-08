8d7b533f8a9aa97a93c1bf2c0b6d83874183fba7
esutorius <etws@roe.ac.uk>
2021-12-08 12:55:35 +0000

    Added new classes fro patients, doctors, etc

--- inflammation/models.py
+++ inflammation/models.py
@@ -10,6 +10,58 @@ and each column represents a single day across all patients.
 import numpy as np
 
 
+class Observation:
+    def __init__(self, day, value):
+        self.day = day
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Doctor(Person):
+    def __init__(self, name):
+        super().__init__(name)
+        self.patients = []
+
+    def add_patient(self, patient):
+        new_patient = Patient(patient)
+        self.patients.append(new_patient)
+
+        return new_patient
+
+class Patient(Person):
+    def __init__(self, name):
+        super().__init__(name)
+        self.observations = []
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1]['day'] + 1
+
+            except IndexError:
+                day = 0
+
+        new_observation = Observation(value, day)
+
+        self.observations.append(new_observation)
+        return new_observation
+
+
+    @property
+    def last_observation(self):
+        return self.observations[-1]
+
+
 def load_csv(filename):
     """Load a Numpy array from a CSV
 
@@ -73,3 +125,13 @@ def patient_normalise(data):
 # TODO(lesson-design) Add Patient class
 # TODO(lesson-design) Implement data persistence
 # TODO(lesson-design) Add Doctor class
+p = Patient('Alice')
+print(p.name)
+
+obs = p.add_observation(3,1)
+print(obs)
+print(p.observations[0].value)
+d = Doctor("Chris")
+d.add_patient("Alice")
+
+print(d.patients[0].observations)
--- tests/test_patient.py
+++ tests/test_patient.py
@@ -8,3 +8,16 @@ def test_create_patient():
     p = Patient(name=name)
 
     assert p.name == name
+
+
+def test_create_doctor():
+    from inflammation.models import Doctor
+
+    name = 'Chris'
+    d = Doctor(name=name)
+
+    assert d.name == name
+    patient = 'Alice'
+    d.add_patient(patient)
+
+    assert d.patients[0].name == patient
