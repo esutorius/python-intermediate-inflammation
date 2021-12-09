ec259ff70fc3b43bcaa2b381d94727545eabc070
esutorius <etws@roe.ac.uk>
2021-12-09 10:33:18 +0000

    Added test for observations

--- tests/test_patient.py
+++ tests/test_patient.py
@@ -17,7 +17,18 @@ def test_create_doctor():
     d = Doctor(name=name)
 
     assert d.name == name
+
     patient = 'Alice'
     d.add_patient(patient)
 
     assert d.patients[0].name == patient
+
+def test_add_observation():
+    from inflammation.models import Patient
+
+    patient = 'Alice'
+    p = Patient(name=patient)
+    obs = p.add_observation(3)
+
+    assert p.observations[0].day == 0
+    assert p.observations[0].value == 3
